c457fe5eb5e3b41acbac706020acd7e95b1cc8cc
saharhamraoui <sahar.hamraoui@esprit.tn>
2024-05-01 12:43:42 +0100

    tenth commit

--- src/Service/TwilioService.php
+++ src/Service/TwilioService.php
@@ -6,13 +6,13 @@ use Twilio\Rest\Client;
 
 class TwilioService
 {
-    private $accountSid = 'AC7cd4467f76c6d4c0c290404a3c17df84';
-    private $authToken = 'bd2fc656082b0ab5eaee5d181886cd70';
+    private $accountSid = '';
+    private $authToken = '';
     private $twilioPhoneNumber = '+16562262555';
    
     public function sendSMS()
     {
-        $to = '+21654825185'; // Le numéro de téléphone destinataire
+        $to = ''; // Le numéro de téléphone destinataire
         $message = 'you  signed up to our app recruti successfully'; // Le message que vous souhaitez envoyer
         $client = new Client($this->accountSid, $this->authToken);
         $client->messages->create(
